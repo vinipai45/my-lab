fe820d67f31eeac08f12c3e1db9eef01cc229cb8
SHREESHA SHARMA U <shreeshauppangala@gmail.com>
2022-01-12 11:49:31 +0530

    fixes

--- src/editor/Charts-Custom-Plugin.jsx
+++ src/editor/Charts-Custom-Plugin.jsx
@@ -5,70 +5,70 @@ import Googlesheets from "./Google-sheets";
 import CustomDrawer from "./drawer";
 
 export default class Chart extends React.Component {
-    static get toolbox() {
-        return {
-            icon: `<i id="graphIcon" class="far fa-chart-bar"></i>`,
-            title: "Chart",
-        };
-    }
-
-    constructor({ data, api, readOnly }) {
-        super();
-        this.state = {
-            openGraph: false,
-        };
-        this.api = api;
-        this.readOnly = readOnly;
-        // this.data = {
-        //     events: data.events || [],
-        // };
-        this.data = data;
-        this.nodes = {
-            holder: null,
-        };
-        this.attachEventListnerToGraph = this.attachEventListnerToGraph.bind(this);
-        this.setOpenGraph = this.setOpenGraph.bind(this);
-
-        // console.log(this.data, "data")
-    }
-
-    componentDidMount() {
-        console.log("componentDidMount");
-        this.attachEventListnerToGraph();
-    }
-
-    openToolbar() {
-        this.api.toolbar.open();
-        console.log("open");
-    }
-
-    setOpenGraph = () => {
-        this.setState({
-            openGraph: true,
-        });
-        console.log("setState");
+  static get toolbox() {
+    return {
+      icon: `<i id="graphIcon" class="far fa-chart-bar"></i>`,
+      title: "Chart",
     };
+  }
 
-    attachEventListnerToGraph = () => {
-        const element = document.getElementById("graphIcon");
-        if (element) {
-            element.addEventListener("click", this.setOpenGraph);
-        }
+  constructor({ data, api, readOnly }) {
+    super();
+    this.state = {
+      openGraph: false,
     };
+    this.api = api;
+    this.readOnly = readOnly;
+    // this.data = {
+    //     events: data.events || [],
+    // };
+    this.data = data;
+    this.nodes = {
+      holder: null,
+    };
+    this.attachEventListnerToGraph = this.attachEventListnerToGraph.bind(this);
+    this.setOpenGraph = this.setOpenGraph.bind(this);
 
-    render() {
-        const rootNode = document.createElement("div");
-        this.nodes.holder = rootNode;
+    // console.log(this.data, "data")
+  }
 
-        // const onDataChange = (newData) => {
-        //     this.data = {
-        //         ...newData
-        //     };
-        // }
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.attachEventListnerToGraph();
+  }
 
-        ReactDOM.render(
-            <div>
-                {/* <CustomDrawer
+  openToolbar() {
+    this.api.toolbar.open();
+    console.log("open");
+  }
+
+  setOpenGraph = () => {
+    this.setState({
+      openGraph: true,
+    });
+    console.log("setState");
+  };
+
+  attachEventListnerToGraph = () => {
+    const element = document.getElementById("graphIcon");
+    if (element) {
+      element.addEventListener("click", this.setOpenGraph);
+    }
+  };
+
+  render() {
+    const rootNode = document.createElement("div");
+    this.nodes.holder = rootNode;
+
+    // const onDataChange = (newData) => {
+    //     this.data = {
+    //         ...newData
+    //     };
+    // }
+
+    ReactDOM.render(
+      <div>
+        {/* <CustomDrawer
                     openDrawer={this.state.openGraph}
                     setOpenDrawer={this.setOpenGraph}
                     title="add Graph"
@@ -76,15 +76,15 @@ export default class Chart extends React.Component {
                     <Googlesheets />
                 </CustomDrawer> */}
 
-                <RenderCharts type={this.data.chart_type} />
-            </div>,
-            rootNode
-        );
+        <RenderCharts type={this.data.chart_type} />
+      </div>,
+      rootNode
+    );
 
-        return this.nodes.holder;
-    }
+    return this.nodes.holder;
+  }
 
-    save() {
-        return this.data;
-    }
+  save() {
+    return this.data;
+  }
 }
