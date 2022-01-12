e8b609fe5a36bad2b46581c9cc6202303cdb43d0
SHREESHA SHARMA U <shreeshauppangala@gmail.com>
2022-01-12 12:30:17 +0530

    err fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Editor from "./editor/editor";
-import TestRedux from './TestRedux/TestRedux';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
-
           <Route path="/" exact>
             <Editor />
           </Route>
-
-          <Route path="/test-redux" exact>
-            <TestRedux />
-          </Route>
-
         </Switch>
       </BrowserRouter>
     </>
