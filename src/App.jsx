b2240cae183cc8c03257e6a366e732c5ea1ac519
vinipai45 <vinipai45@gmail.com>
2022-01-12 09:36:54 +0530

    redux integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Editor from "./editor/editor";
+import TestRedux from './TestRedux/TestRedux';
 
 const App = () => {
   return (
-    <div>
-      <Editor />
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+
+          <Route path="/" exact>
+            <Editor />
+          </Route>
+
+          <Route path="/test-redux" exact>
+            <TestRedux />
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
