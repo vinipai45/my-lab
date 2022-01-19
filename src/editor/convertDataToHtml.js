1c00548a70a3790e2ed3b9fdb889d4ae2b9b920e
vinipai45 <vinipai45@gmail.com>
2022-01-19 10:57:32 +0530

    table html logic

--- src/editor/convertDataToHtml.js
+++ src/editor/convertDataToHtml.js
@@ -36,7 +36,27 @@ const convertDataToHtml = (blocks) => {
                 console.log('Drawer')
                 break;
             case "table":
-                console.log('table');
+                convertedHtml += `
+                <table border=1 cellpadding=10 style="border-collapse:collapse">
+                ${block.data.content.map((row, index) => {
+                    if (block.data.withHeadings) {
+                        if (index === 0) {
+                            return `<tr>${row.map(item => {
+                                return `<th>${item}</th>`
+                            }).join('')}</tr>`
+                        } else {
+                            return `<tr>${row.map(item => {
+                                return `<td>${item}</td>`
+                            }).join('')}</tr>`
+                        }
+                    } else {
+                        return `<tr>${row.map(item => {
+                            return `<td>${item}</td>`
+                        }).join('')}</tr>`
+                    }
+                }).join('')}
+                </table>
+                `
                 break;
             default:
                 console.log("Unknown block type", block.type);
