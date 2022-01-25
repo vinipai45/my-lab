96999b3ac924bdb608745de9b5f9ce43cdde3a7a
vinipai45 <vinipai45@gmail.com>
2022-01-25 16:48:25 +0530

    google sheets data to json

--- src/button.js
+++ src/button.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import { useEffect } from 'react';
-import useDrivePicker from 'react-google-drive-picker'
-
 import { gapi } from 'gapi-script';
+import { useEffect, useState } from 'react';
+import useDrivePicker from 'react-google-drive-picker'
+// import { gapi } from 'gapi-script';
+let CLIENT_ID = "487053781358-2jl9tlbjh3nn5c8mtldmqk652qo67l4o.apps.googleusercontent.com";
+let API_KEY = "AIzaSyDUdE47cYyovi8RXJAAxCtXydQe6TvCbfQ";
 
 function App() {
     const [openPicker, data, authResponse,] = useDrivePicker();
     // const customViewsArray = [new google.picker.DocsView()]; // custom view
+
+
     const handleOpenPicker = () => {
         openPicker({
-            clientId: "487053781358-2jl9tlbjh3nn5c8mtldmqk652qo67l4o.apps.googleusercontent.com",
-            developerKey: "AIzaSyDUdE47cYyovi8RXJAAxCtXydQe6TvCbfQ",
+            clientId: CLIENT_ID,
+            developerKey: API_KEY,
             viewId: "SPREADSHEETS",
             // token: token, // pass oauth token in case you already have one
             showUploadView: false,
@@ -26,26 +30,16 @@ function App() {
     useEffect(() => {
         // do anything with the selected/uploaded files
         if (data) {
-            data.docs.map(i => {
-                console.log(i)
-                // gapi.client.sheets.spreadsheets.values.get({
-                //     spreadsheetId: i.id,
-                //     range: ['A1']
-                // }).then((response) => {
-                //     var result = response.result;
-                //     var numRows = result.values ? result.values.length : 0;
-                //     console.log(`${numRows} rows retrieved.`);
-                // });
-                // axios.get(i.embedUrl).then(data => {
-
-                // })
+            data.docs.map(doc => {
+                console.log(doc)
+                gapi.client.sheets.spreadsheets.values.get({
+                    spreadsheetId: doc.id,
+                    range: 'A1:Z',
+                }).then(res => console.log(JSON.parse(res.body)))
             })
         }
     }, [data])
 
-    useEffect(() => {
-        console.log(gapi, "gapi");
-    }, [])
 
 
     return (
